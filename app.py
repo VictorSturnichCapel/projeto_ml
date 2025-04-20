b2bdac981ed47c53c1d120819406f10b8218f7d3
= <victorsturnichcapel@gmail.com>
2025-04-20 08:49:34 -0300

    atualizações

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ st.divider()
 diametro = st.number_input('Diâmetro da pizza (cm)', min_value=0)
 
 if diametro:
+    
     preco_previsto = modelo.predict([[diametro]])[0][0]
     st.write(f'O preço previsto para uma pizza de {diametro} cm é R$ {preco_previsto:.2f}')
     st.balloons()
